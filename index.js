fec44ab5199db6353601bedba8f262bed56cd19e
congminh220720 <ncongminh91@gmail.com>
2022-02-28 16:48:23 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const findMyState = () => {
   const status  = document.querySelector('.status')
 
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 6000,
+    maximumAge: 0
+  };
+
   const success = (postion) => {
     const latitude = postion.coords.latitude
     const longtitude = postion.coords.longitude
     const location = latitude.toString() + ',' + longtitude.toString();
-    console.log(location)
+
+    console.log('Location:',location)
+    console.log(postion)
+
+    status.textContent = `${location}`
+
 
     const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longtitude}&localityLanguage=en`
 
@@ -13,7 +24,7 @@ const findMyState = () => {
       return res.json()
     }).then((data) => {
       
-      console.log(data)
+      console.log('detail:',data)
     })
 
   }
@@ -21,7 +32,7 @@ const findMyState = () => {
   const error = () => {
     status.textContent = 'Unable to retireve your location'
   }
-  navigator.geolocation.getCurrentPosition(success,error);
+  navigator.geolocation.getCurrentPosition(success,error,options);
 
 }
 document.querySelector('.find-state').addEventListener('click', findMyState)
